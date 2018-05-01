ee7351599681fa63d386d0f0408ac82b6088fbf5
Mortim <33499834+mortim59@users.noreply.github.com>
2018-05-01 17:16:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ bot.on('message', function(message) {
 	
 		var embed = new Discord.RichEmbed()
 		.setColor("#226666")
-		.addField("Voici votre profil: ", "Pseudo: **" + message.author.username + "**\nIdentifiant: **" + message.author.id + "**\nStatut: **"  + message.author.presence.status + "**\nCe compte a été crée le **" + date_moment.format('DD MMM YYYY') + "**\nVous etes en train de jouer a **" + message.author.presence.game + "**")
+		.addField("Voici votre profil: ", "Pseudo: **" + message.author.username + "**\nIdentifiant: **" + message.author.id + "**\nStatut: **"  + message.author.presence.status + "**\nCe compte a été crée le **" + date_moment.format('DD MMM YYYY') + "**\nVous etes en train de jouer a **" + if(message.author.presence.game.name == null) { "**Vous ne jouez a aucune activité**" } else { "**" + message.author.presence.game.name + "**" })
 		.setFooter('Les meilleurs profils de DevBot')
 		.setThumbnail(message.author.avatarURL)
 		message.channel.send(embed).catch(console.error)
