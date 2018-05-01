f2890a2eae285fb326d54af839e85014498d49fd
Mortim <33499834+mortim59@users.noreply.github.com>
2018-05-01 17:24:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -102,7 +102,11 @@ bot.on('message', function(message) {
 	
 		var embed = new Discord.RichEmbed()
 		.setColor("#226666")
-		.addField("Voici votre profil: ", "Pseudo: **" + message.author.username + "**\nIdentifiant: **" + message.author.id + "**\nStatut: **"  + message.author.presence.status + "**\nCe compte a été crée le **" + date_moment.format('DD MMM YYYY') + if(message.author.presence.game.name == null) { "**Vous ne jouez a aucune activité**" } else { "**" + message.author.presence.game.name + "**" })
+		if(message.author.presence.game.name == null) {
+		  .addField("Voici votre profil: ", "Pseudo: **" + message.author.username + "**\nIdentifiant: **" + message.author.id + "**\nStatut: **"  + message.author.presence.status + "**\nCe compte a été crée le **" + date_moment.format('DD MMM YYYY') + "**\nVous ne jouez à aucune activité**")
+		} else {
+		  .addField("Voici votre profil: ", "Pseudo: **" + message.author.username + "**\nIdentifiant: **" + message.author.id + "**\nStatut: **"  + message.author.presence.status + "**\nCe compte a été crée le **" + date_moment.format('DD MMM YYYY') + "\nVous jouez à **" + message.author.presence.game.name + "**")
+		}
 		.setFooter('Les meilleurs profils de DevBot')
 		.setThumbnail(message.author.avatarURL)
 		message.channel.send(embed).catch(console.error)
