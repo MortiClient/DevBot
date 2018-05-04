321952d77a4b06286ec35cfbaef70e4c17ba71d6
Mortim <33499834+mortim59@users.noreply.github.com>
2018-05-04 17:45:26 +0200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -9,7 +9,9 @@ module.exports = class Say extends Command {
 	}
 
 	static action(message) {
-			
+		
+			let args = message.content.split(" ").slice(1);
+		
 			message.delete()
 			var embed = new Discord.RichEmbed()
 			.setColor("#226666")
