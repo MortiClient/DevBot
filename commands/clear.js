56b5a111dd793a19f3929330529edaab68d073e7
Mortim <33499834+mortim59@users.noreply.github.com>
2018-05-04 17:23:40 +0200

    Add files via upload

---
+++ commands/clear.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js')
+const Command = require('./command')
+const prefix = 'd?'
+
+module.exports = class Clear extends Command {
+
+	static match(message) {
+		return message.content.startsWith(prefix + 'clear')
+	}
+
+	static action(message) {
+			
+			 if(!message.member.hasPermission("MANAGE_MESSAGES")) {
+
+		     	return message.reply('Vous n\'avez pas acces a cette commande')
+
+		  		} else {
+
+	          		message.channel.bulkDelete(args[0]).then(() => {
+			  		message.channel.send(args[0] + " messages on été effacés.").then(message => message.delete(5000));
+
+		    	})
+		 	 }
+		}
+}
---
+++ commands/say.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js')
+const Command = require('./command')
+const prefix = 'd?'
+
+module.exports = class Say extends Command {
+
+	static match(message) {
+		return message.content.startsWith(prefix + 'say')
+	}
+
+	static action(message) {
+			
+			message.delete()
+			var embed = new Discord.RichEmbed()
+			.setColor("#226666")
+			.setDescription("**" + message.author.username + "**" + " a dit: " + " " + args.join(" "))
+			message.channel.send(embed)
+	}
+}
